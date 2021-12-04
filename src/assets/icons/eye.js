7194707e083956120a7dbe2933cfc8e118997488
Alireza <alirezaaskarpourr@gmail.com>
2021-12-04 22:46:30 +0330

    add auth pages

---
+++ src/assets/icons/eye.js
@@ -0,0 +1,19 @@
+const eye = (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    className="feather feather-eye"
+    viewBox="0 0 24 24"
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+)
+
+export default eye

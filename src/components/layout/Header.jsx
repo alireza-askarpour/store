1cf8468accb4fa2ce092a5c1364db1dbd2be9d11
Alireza Askarpour <alirezaaskarpourr@gmail.com>
2022-05-17 05:41:35 -0400

    refactor: improve header component in layout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-
 import { useLayout } from '../../providers/layout'
+import { classNames } from '../../utils/classNames'
 
 const Header = ({ children }) => {
   const { menuLayout, menuCollaps } = useLayout()
 
-  const menuCollapss =
-    menuLayout === 'vertical' && menuCollaps
-      ? 'menu-collaps'
-      : menuLayout === 'vertical' && !menuCollaps
-      ? 'menu-open'
-      : ''
-
-  return <header className={`header ${menuCollapss}`}>{children}</header>
+  return (
+    <header
+      className={classNames(
+        'header',
+        menuLayout === 'vertical' && menuCollaps && 'menu-collaps',
+        menuLayout === 'vertical' && !menuCollaps && 'menu-open',
+      )}
+    >
+      {children}
+    </header>
+  )
 }
 
 export default Header

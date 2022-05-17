9b10f21f7f71e346c8a320ce9f9730f9afa843e9
Alireza Askarpour <alirezaaskarpourr@gmail.com>
2022-05-17 06:07:57 -0400

    refactor: improve navbar main in layout

--- src/components/layout/navbarMenu/NavbarMenu.jsx
+++ src/components/layout/navbarMenu/NavbarMenu.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
+import { classNames } from '../../../utils/classNames'
 import { sidebarMenu } from '../../../assets/data/navbar_menu'
 
 const NavbarMenu = () => {
   const { pathname } = useLocation()
-
   const activeItem = sidebarMenu.findIndex((item) => item.route === pathname)
 
   return (
@@ -14,8 +14,11 @@ const NavbarMenu = () => {
         <ul className="navbar-menu-items">
           {sidebarMenu.map((item, index) => (
             <li
-              className={`nav-item ${index === activeItem ? 'active' : ''}`}
               key={index}
+              className={classNames(
+                'nav-item', 
+                index === activeItem && 'active'
+              )}
             >
               <Link to={item.route}>
                 {item.icon}
